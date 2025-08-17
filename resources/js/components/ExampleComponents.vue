d5ee5f1ad8dd175bdd0f9b0f5349b19d910eefe2
SirMatteo <baloghszabomate@gmail.com>
2025-08-17 15:40:27 +0200

    Vue hozzáadva

---
+++ resources/js/components/ExampleComponents.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Hello Vue a Laravelben 🎉</h1>
+        <button @click="count++">Klikkelve: {{ count }}x</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
